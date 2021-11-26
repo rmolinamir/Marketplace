b8b53734856f231e52f448cc60875aafa41c6dcb
Robert Molina <rmolinamir@gmail.com>
2021-11-26 18:32:57 -0500

    Add project files.

---
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -0,0 +1,24 @@
+﻿namespace Marketplace.Domain
+{
+    public class ClassifiedAd
+    {
+        public Guid Id { get; }
+
+        public ClassifiedAd(Guid id)
+        {
+            if (id == default) throw new ArgumentNullException(nameof(id), "Idendity must be specified");
+            Id = id;
+        }
+
+        public void SetTitle(string title) => _title = title;
+
+        public void UpdateText(string text) => _text = text;
+
+        public void UpdatePrice(decimal price) => _price = price;
+
+        private Guid _ownerId;
+        private string _title;
+        private string _text;
+        private decimal _price;
+    }
+}
